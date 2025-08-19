8e38af68b569895e056bdfeaef0b22481825697b
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-08-19 17:58:40 +0530

    constructor

---
+++ OOPs/three.cpp
@@ -0,0 +1,69 @@
+//Constructor: automatically  called when creating a object
+// compiler can itself create a constructor
+//constructor functions have same name as class
+// they dont have a return type;
+//called automatically once at object creation
+//memory allocation happend when constructor is called 
+//classes do not have memory memory only allocated to objects
+#include<bits/stdc++.h>
+using namespace std;
+
+class Teacher {
+private: 
+    int salary;
+public:
+    //constructor: Non-paramatrizes
+    Teacher() {
+        cout<<"HI, Im a constructor" <<endl;
+        college="DJSCE";
+    }
+
+    //constructor:paramatrizes
+    Teacher(string name, string dept, string college, string subject) {
+        this->name=name;
+        this->dept=dept;
+        this->college=college;
+        this->subject=subject;
+    }
+
+    //copy constructor
+    Teacher(Teacher &orgOb) {
+        this->name=orgOb.name;
+    }
+
+    string name;
+    string dept;
+    string college;
+    string subject;
+
+    void changeDept(string newDept) {
+        dept=newDept;
+    }
+
+    void setSalary(int newSalary) {
+        salary=newSalary;
+    }
+    
+    int getSalary() {
+        return salary;
+    }
+};
+
+int main() {
+    Teacher t1("Divya","IT","DJSCE","Maths");
+    Teacher t2;
+    Teacher t3(t1);
+
+    t1.name= "divya";
+    t1.dept="computer";
+    t1.changeDept("IT");
+    t1.setSalary(50000);
+
+    cout<<t1.getSalary()<<endl;
+
+    cout<<t1.subject<<endl;
+    cout << "name " << t2.name;
+
+
+    return 0;
+}
---
+++ OOPs/two.cpp
@@ -0,0 +1,21 @@
+//Encapsulatio: wrapping up of data and 
+    //member functions in a single using called class.
+
+//helps to hide sensitive information by making private
+#include<bits/stdc++.h>
+using namespace std;
+
+class Account {
+private:
+    string password;
+    string acBalance; //data hiding
+
+public:
+    string name;
+    string type;
+
+};
+
+int main() {
+    return 0;
+}
