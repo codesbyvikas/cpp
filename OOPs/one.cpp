b7a88a1729a535100bcfd346c6ff54aaff19bdf5
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-08-19 17:00:10 +0530

    classes

--- OOPs/one.cpp
+++ OOPs/one.cpp
@@ -7,6 +7,12 @@
 using namespace std;
 
 class Teacher {
+private: 
+    int salary;
+public:
+//public: data and methods are accessible to everyone
+//private: only inside class
+//protected: inside class and to its derived class
     //properties/attributes
     string name;
     string dept;
@@ -16,4 +22,32 @@ class Teacher {
     void changeDept(string newDept) {
         dept=newDept;
     }
+
+    //setter: to set private values
+    void setSalary(int newSalary) {
+        salary=newSalary;
+    }
+    
+    //getter: to get private values
+    int getSalary() {
+        return salary;
+    }
 };
+
+int main() {
+    Teacher t1;
+    Teacher t2;
+    Teacher t3;
+
+    t1.name= "divya";
+    t1.dept="computer";
+    t1.changeDept("IT");
+    t1.setSalary(50000);
+
+    cout<<t1.getSalary()<<endl;
+
+    cout<<t1.dept<<endl;
+
+
+    return 0;
+}
